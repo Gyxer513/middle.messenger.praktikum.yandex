30ae5f66235741d061f87d20dc48e38271a4c684
Gyxer513 <102280477+Gyxer513@users.noreply.github.com>
2024-04-24 23:04:56 +0300

    fia TS problem

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,14 @@ import Handlebars from 'handlebars';
 import { Button } from '../src/parcials/components/button/index.ts';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const root = document.querySelector('#app');
+  const root: HTMLElement | null = document.querySelector('#app');
+  if (root !== null) {
+    const template = Handlebars.compile(Button);
 
-  const template = Handlebars.compile(Button);
+    const result = template({ class: `button button_main`, text: 'Нажми мне' });
 
-  const result = template({ class: `button button_main`, text: 'Нажми мне' });
-
-  root.innerHTML = result;
+    root.innerHTML = result;
+  } else {
+    console.error('#app не найден');
+  }
 });
