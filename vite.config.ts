b56ff01dd8baa7b66debfded8ea8176d2caf0211
Gyxer513 <102280477+Gyxer513@users.noreply.github.com>
2024-04-30 14:29:28 +0300

    added new line in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,1 @@
-import { defineConfig } from 'vite';
-import { resolve } from 'path';
-
-export default defineConfig({
-  server: {
-    port: 3000
-  },
-  root: resolve(__dirname, 'src'),
-  build: {
-    outDir: resolve(__dirname, 'dist')
-  },
-  resolve: {
-    alias: {
-      '@': '/src'
-    }
-  }
-})
-
+import { defineConfig } from 'vite';import { resolve } from 'path';export default defineConfig({  server: {    port: 3000  },  root: resolve(__dirname, 'src'),  build: {    outDir: resolve(__dirname, 'dist')  },  resolve: {    alias: {      '@': '/src'    }  }})
